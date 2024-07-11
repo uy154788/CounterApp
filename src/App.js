3f71b94fd7de0fd29b7f57eb971b6c096c0e0406
uy154788 <118651983+uy154788@users.noreply.github.com>
2024-07-11 22:31:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
+      <div>Increment & Decrement</div>
+      <div>
+        <button>
+          -
+        </button>
+        <div>
+        </div>
+        <button>
+          +
+        </button>
+      </div>
+      <button>
+        reset
+      </button>
     </div>
   );
 }
